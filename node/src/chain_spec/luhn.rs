fb03f70de826014394cd36eaa3675e6216c94672
Tlalocman <desneruda@gmail.com>
2023-11-02 16:05:00 -0600

    🔧 chore(luhn.rs): remove unused imports and reformat code for better readability ✨ feat(luhn.rs): add support for configurable token symbol, decimals, ss58Format, prefix, network, displayName, standardAccount, and website properties in chain specification 🔧 chore(luhn.rs): reformat code for better readability ✨ feat(luhn.rs): add support for multiple collators and PH in chain specification 🔧 chore(luhn.rs): reformat code for better readability ✨ feat(luhn.rs): add support for hashed genesis configuration in chain specification 🔧 chore(luhn.rs): reformat code for better readability

--- node/src/chain_spec/luhn.rs
+++ node/src/chain_spec/luhn.rs
@@ -1,11 +1,12 @@
 use sc_service::ChainType;
 // use sp_core::sr25519;
-use sp_core::{crypto::UncheckedInto};
+use sp_core::crypto::UncheckedInto;
 
 use hex_literal::hex;
 
 use super::{
-	/*get_account_id_from_seed, get_collator_keys_from_seed,*/ session_keys, SAFE_XCM_VERSION, Extensions,
+	/*get_account_id_from_seed, get_collator_keys_from_seed,*/ session_keys, Extensions,
+	SAFE_XCM_VERSION,
 };
 
 use cumulus_primitives_core::ParaId;
@@ -17,15 +18,14 @@ pub type HashedChainSpec =
 
 /// Gen HASH chain specification
 pub fn get_chain_spec() -> HashedChainSpec {
-
-    let mut properties = sc_chain_spec::Properties::new();
+	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("tokenSymbol".into(), "LUHN".into());
 	properties.insert("tokenDecimals".into(), 18.into());
 	properties.insert("ss58Format".into(), 11486.into());
 	properties.insert("prefix".into(), 11486.into());
 	properties.insert("network".into(), "luhn".into());
 	properties.insert("displayName".into(), "Luhn Network".into());
-	properties.insert("standardAccount".into(),"*25519".into());
+	properties.insert("standardAccount".into(), "*25519".into());
 	properties.insert("website".into(), "https://luhn.network".into());
 
 	HashedChainSpec::from_genesis(
@@ -38,21 +38,25 @@ pub fn get_chain_spec() -> HashedChainSpec {
 				vec![
 					(
 						// Collator #1
-                        // uhsPQGuXYwjnLvoJWWttQ6FEVtztHSvsjE7UFzxS8mSfoSmts
-                        hex!["1cfc7e49e91696b84bf8e931c16375ea634c3997b36155657faf7dc4716e273e"].into(),
-                        hex!["1cfc7e49e91696b84bf8e931c16375ea634c3997b36155657faf7dc4716e273e"].unchecked_into(),
+						// uhsPQGuXYwjnLvoJWWttQ6FEVtztHSvsjE7UFzxS8mSfoSmts
+						hex!["1cfc7e49e91696b84bf8e931c16375ea634c3997b36155657faf7dc4716e273e"]
+							.into(),
+						hex!["1cfc7e49e91696b84bf8e931c16375ea634c3997b36155657faf7dc4716e273e"]
+							.unchecked_into(),
 					),
 					(
 						// Collator #2
-                        // uhujXWvqSqGrY3K62qeamwCGQd7tTo1hm1s9ZYrgxAZFBLyLv
-                        hex!["84ce3f0bc9ae73d8497c6161927e9e04f39f4bc54579689532d048188c10a77c"].into(),
-                        hex!["84ce3f0bc9ae73d8497c6161927e9e04f39f4bc54579689532d048188c10a77c"].unchecked_into(),
+						// uhujXWvqSqGrY3K62qeamwCGQd7tTo1hm1s9ZYrgxAZFBLyLv
+						hex!["84ce3f0bc9ae73d8497c6161927e9e04f39f4bc54579689532d048188c10a77c"]
+							.into(),
+						hex!["84ce3f0bc9ae73d8497c6161927e9e04f39f4bc54579689532d048188c10a77c"]
+							.unchecked_into(),
 					),
 				],
 				vec![
 					// PH
 					// uhtqJBJ9ZeKguyAG4GJ2S7cme5FvJ661P5NVdHTYKQgvDEQAR
-                    hex!["5cf8957922e4058a953281f82fdced2e4d389fe37c77f41a0fd2379df0caf877"].into(),
+					hex!["5cf8957922e4058a953281f82fdced2e4d389fe37c77f41a0fd2379df0caf877"].into(),
 				],
 				// uhtqJBJ9ZeKguyAG4GJ2S7cme5FvJ661P5NVdHTYKQgvDEQAR
 				hex!["5cf8957922e4058a953281f82fdced2e4d389fe37c77f41a0fd2379df0caf877"].into(),
@@ -64,10 +68,7 @@ pub fn get_chain_spec() -> HashedChainSpec {
 		None,
 		None,
 		Some(properties),
-		Extensions {
-			relay_chain: "kusama".into(),
-			para_id: 2232,
-		},
+		Extensions { relay_chain: "kusama".into(), para_id: 2232 },
 	)
 }
 
@@ -84,7 +85,11 @@ fn hashed_genesis(
 				.to_vec(),
 		},
 		balances: hashed_parachain_runtime::BalancesConfig {
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1000000000000000000000000000)).collect(),
+			balances: endowed_accounts
+				.iter()
+				.cloned()
+				.map(|k| (k, 1000000000000000000000000000))
+				.collect(),
 		},
 		transaction_payment: Default::default(),
 		sudo: SudoConfig { key: Some(root_key) },
@@ -101,8 +106,8 @@ fn hashed_genesis(
 				.into_iter()
 				.map(|(acc, aura)| {
 					(
-						acc.clone(),                 // account id
-						acc,                         // validator id
+						acc.clone(),        // account id
+						acc,                // validator id
 						session_keys(aura), // session keys
 					)
 				})
@@ -118,4 +123,3 @@ fn hashed_genesis(
 		},
 	}
 }
-
